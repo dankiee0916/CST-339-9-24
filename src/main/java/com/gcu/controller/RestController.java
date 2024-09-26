ca121c944c6a62790293891d9d9108788a667a76
LoganSwinson <97139851+LoganSwinson@users.noreply.github.com>
2024-09-26 15:13:29 -0700

    Changed the Hello World! message

--- src/main/java/com/gcu/controller/RestController.java
+++ src/main/java/com/gcu/controller/RestController.java
@@ -13,6 +13,6 @@ public class RestController {
     @GetMapping("/test1")
     @ResponseBody
     public String printHello() {
-        return "Hello World!";
+        return "Hello World! From a merged branch! :)";
     }
 }
